f2ea8cb4780f268e9139d7a9923ca7361bfc1dec
fayax555 <fayax555@gmail.com>
2021-06-01 21:04:23 +0500

    testing heroku api

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const puppeteer = require('puppeteer');
 const app = express();
 const port = process.env.PORT || 3000;
 
+var allowCrossDomain = function (req, res, next) {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, Content-Length, X-Requested-With'
+   );
+
+   // intercept OPTIONS method
+   if ('OPTIONS' == req.method) {
+      res.send(200);
+   } else {
+      next();
+   }
+};
+
+app.use(allowCrossDomain);
+
 app.get('/', async (req, res) => {
    const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
