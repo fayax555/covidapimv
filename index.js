eba254fde55653ca436224e561ecef81a4f20ad3
fayax555 <fayax555@gmail.com>
2021-06-01 19:32:05 +0500

    testing api

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+let port = process.env.PORT || 3000;
+let covidDataJson = '';
+
+(async () => {
+   const browser = await puppeteer.launch();
+   const page = await browser.newPage();
+
+   await page.goto('https://covid19.health.gov.mv/en/?c=0', {
+      waitUntil: 'networkidle0',
+      timeout: 0,
+   });
+
+   // first col
+   let [totalCasesEl] = await page.$x('//*[@id="cases_total"]');
+   let [activeCasesEl] = await page.$x('//*[@id="cases_active"]');
+   let [recoveriesEl] = await page.$x('//*[@id="cases_recovered"]');
+   let [totalDeathsEl] = await page.$x('//*[@id="cases_deaths"]');
+
+   const totalCasesJson = await (
+      await totalCasesEl.getProperty('textContent')
+   ).jsonValue();
+   const activeCasesJson = await (
+      await activeCasesEl.getProperty('textContent')
+   ).jsonValue();
+   const recoveriesJson = await (
+      await recoveriesEl.getProperty('textContent')
+   ).jsonValue();
+   const totalDeathsJson = await (
+      await totalDeathsEl.getProperty('textContent')
+   ).jsonValue();
+
+   covidDataJson = [
+      {
+         total: totalCasesJson,
+         activeCases: activeCasesJson,
+         recoveries: recoveriesJson,
+         totalDeaths: totalDeathsJson,
+      },
+   ];
+   covidjson = JSON.stringify(covidDataJson);
+   app.get('/', (req, res) => {
+      res.send(JSON.stringify(covidjson));
+   });
+
+   app.listen(port, () => {
+      console.log(`port listening on http://localhost:${port}`);
+   });
+
+   browser.close();
+})();
--- scrape.js
+++ scrape.js
@@ -1,52 +1,32 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
-(async () => {
-   const browser = await puppeteer.launch();
-   const page = await browser.newPage();
+// (async () => {
+//    const browser = await puppeteer.launch();
+//    const page = await browser.newPage();
 
-   await page.goto('https://covid19.health.gov.mv/en/?c=0', {
-      waitUntil: 'networkidle0',
-      timeout: 0,
-   });
+//    await page.goto('https://covid19.health.gov.mv/en/?c=0', {
+//       waitUntil: 'networkidle0',
+//       timeout: 0,
+//    });
 
-   // first col
-   let [totalCasesEl] = await page.$x('//*[@id="cases_total"]');
-   let [activeCasesEl] = await page.$x('//*[@id="cases_active"]');
-   let [recoveriesEl] = await page.$x('//*[@id="cases_recovered"]');
-   let [totalDeathsEl] = await page.$x('//*[@id="cases_deaths"]');
-   let [hospitalizedEl] = await page.$x('//*[@id="cases_critical"]');
+//    // first col
+//    let [totalCasesEl] = await page.$x('//*[@id="cases_total"]');
+//    let [activeCasesEl] = await page.$x('//*[@id="cases_active"]');
+//    let [recoveriesEl] = await page.$x('//*[@id="cases_recovered"]');
+//    let [totalDeathsEl] = await page.$x('//*[@id="cases_deaths"]');
 
-   const txtTotalCasesEl = await totalCasesEl.getProperty('textContent');
-   const txtActiveCasesEl = await activeCasesEl.getProperty('textContent');
-   const txtRecoveriesEl = await recoveriesEl.getProperty('textContent');
-   const txtTotalDeathsEl = await totalDeathsEl.getProperty('textContent');
-   const hospitalizedEl = await hospitalizedEl.getProperty('textContent');
+//    const totalCasesJson = await (
+//       await totalCasesEl.getProperty('textContent')
+//    ).jsonValue();
+//    const activeCasesJson = await (
+//       await activeCasesEl.getProperty('textContent')
+//    ).jsonValue();
+//    const recoveriesJson = await (
+//       await recoveriesEl.getProperty('textContent')
+//    ).jsonValue();
+//    const totalDeathsJson = await (
+//       await totalDeathsEl.getProperty('textContent')
+//    ).jsonValue();
 
-   const totalCasesJson = await txtTotalCasesEl.jsonValue();
-   const activeCasesJson = await txtActiveCasesEl.jsonValue();
-   const recoveriesJson = await txtRecoveriesEl.jsonValue();
-   const totalDeathsJson = await txtTotalDeathsEl.jsonValue();
-   const totalDeathsJson = await hospitalizedEl.jsonValue();
-
-   // second col: daily new cases
-   let [newCasesToday] = await page.$x('//*[@id="new_cases_today"]');
-   let [maleArea] = await page.$x('//*[@id="today_male_area"]');
-   let [atolls] = await page.$x('//*[@id="today_atolls"]');
-   let [resorts] = await page.$x('//*[@id="today_resorts"]');
-   let [liveaboards] = await page.$x(
-      '//*[@id="today_other_tourist_establishments"]'
-   );
-   let [islandsUnderDevelopment] = await page.$x(
-      '//*[@id="today_islands_under_development"]'
-   );
-   let [industrialIslands] = await page.$x(
-      '//*[@id="today_industrial_islands"]'
-   );
-
-   console.log(totalCasesJson);
-   console.log(activeCasesJson);
-   console.log(recoveriesJson);
-   console.log(totalDeathsJson);
-
-   browser.close();
-})();
+//    browser.close();
+// })();
