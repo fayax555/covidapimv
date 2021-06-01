1c7152e4b001be3d8807c8f344d875097701abde
fayax555 <fayax555@gmail.com>
2021-06-01 20:38:11 +0500

    h

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', async (req, res) => {
    const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
    });
+
    const page = await browser.newPage();
 
    await page.goto('https://covid19.health.gov.mv/en/?c=0', {
