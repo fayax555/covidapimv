9792f13187c27d1612d123d71cf5b361cf599fbf
fayax555 <fayax555@gmail.com>
2021-06-20 15:17:21 +0500

    removing puppeteer

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const app = express();
 const port = process.env.PORT || 3000;
 
-var allowCrossDomain = function (req, res, next) {
+const allowCrossDomain = function (req, res, next) {
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
    res.header(
@@ -24,52 +24,7 @@ var allowCrossDomain = function (req, res, next) {
 app.use(allowCrossDomain);
 
 app.get('/', async (req, res) => {
-   res.send(
-      'COVID API. /gov for data from covid19mv dashboard and /mihaaru for data avalable at mihaaru.com'
-   );
-});
-
-app.get('/gov', async (req, res) => {
-   const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-   });
-
-   const page = await browser.newPage();
-
-   await page.goto('https://covid19.health.gov.mv/en/?c=0', {
-      waitUntil: 'networkidle0',
-      timeout: 0,
-   });
-
-   // first col
-   let [totalCasesEl] = await page.$x('//*[@id="cases_total"]');
-   let [activeCasesEl] = await page.$x('//*[@id="cases_active"]');
-   let [recoveriesEl] = await page.$x('//*[@id="cases_recovered"]');
-   let [totalDeathsEl] = await page.$x('//*[@id="cases_deaths"]');
-
-   const totalCasesJson = await (
-      await totalCasesEl.getProperty('textContent')
-   ).jsonValue();
-   const activeCasesJson = await (
-      await activeCasesEl.getProperty('textContent')
-   ).jsonValue();
-   const recoveriesJson = await (
-      await recoveriesEl.getProperty('textContent')
-   ).jsonValue();
-   const totalDeathsJson = await (
-      await totalDeathsEl.getProperty('textContent')
-   ).jsonValue();
-
-   const covidDataJson = {
-      total: totalCasesJson,
-      activeCases: activeCasesJson,
-      recoveries: recoveriesJson,
-      totalDeaths: totalDeathsJson,
-   };
-
-   await browser.close();
-
-   res.json(covidDataJson);
+   res.send('COVID API. Go to /mihaaru - covid data avalable at mihaaru.com');
 });
 
 app.get('/mihaaru', async (req, res) => {
